d0b7a38391c8288e869a584dd4e847cc718b3e3a
DimasRafsanjani <dimassraf@gmail.com>
2026-08-14 16:23:22 +0700

    chore: setup android app

---
+++ frontend/capacitor.config.ts
@@ -0,0 +1,13 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.dprd.mosque',
+  appName: 'Masjid Asy Syura',
+  webDir: 'dist',
+  server: {
+    url: 'http://masjid.perdinkeuangan.online',
+    cleartext: true
+  }
+};
+
+export default config;
